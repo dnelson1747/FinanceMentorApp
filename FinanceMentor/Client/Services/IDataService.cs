dcc2a4696dbee043d9605896a1bd6444c3c80b8b
dnelson1747 <dnelson1747@gmail.com>
2021-05-14 13:45:29 -0700

    added Radzen component (charts)

---
+++ FinanceMentor/Client/Services/IDataService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinanceMentor.Client.Services
+{
+    public interface IDataService
+    {
+        Task<ICollection<YearlyItem>> LoadCurrentYearExpenses();
+        Task<ICollection<YearlyItem>> LoadCurrentYearEarnings();
+
+        Task<ThreeMonthsData> LoadLast3MonthsExpenses();
+        Task<ThreeMonthsData> LoadLast3MonthsEarnings();
+    }
+}
